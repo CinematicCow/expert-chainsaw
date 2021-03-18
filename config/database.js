2f3b031768fa4e6fe7bf4338acaf4a0ab4b1dd98
CinematicCow <prajwalkakkar@gmail.com>
2021-03-19 02:24:22 +0530

    added: Trainers

--- config/database.js
+++ config/database.js
@@ -1,26 +1,48 @@
 const { parse } = require("pg-connection-string");
+require("dotenv").config();
 
-module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+if (process.env.DATABASE_URL) {
+  module.exports = ({ env }) => {
+    const { host, port, database, user, password } = parse(env("DATABASE_URL"));
 
-  return {
+    return {
+      defaultConnection: "default",
+      connections: {
+        default: {
+          connector: "bookshelf",
+          settings: {
+            client: "postgres",
+            host,
+            port,
+            database,
+            username: user,
+            password,
+            ssl: { rejectUnauthorized: false },
+          },
+          options: {
+            ssl: false,
+          },
+        },
+      },
+    };
+  };
+} else {
+  module.exports = ({ env }) => ({
     defaultConnection: "default",
     connections: {
       default: {
         connector: "bookshelf",
         settings: {
           client: "postgres",
-          host,
-          port,
-          database,
-          username: user,
-          password,
-          ssl: { rejectUnauthorized: false },
+          host: env("DATABASE_HOST"),
+          port: env.int("DATABASE_PORT"),
+          database: env("DATABASE_NAME"),
+          username: env("DATABASE_USERNAME"),
+          password: env("DATABASE_PASSWORD"),
+          ssl: env.bool("DATABASE_SSL"),
         },
-        options: {
-          ssl: false,
-        },
+        options: {},
       },
     },
-  };
-};
+  });
+}
